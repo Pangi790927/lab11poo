7382297d88eb373561bfdfb31a5e176fdc668a0a
Fooxiee <35177394+Fooxiee@users.noreply.github.com>
2018-01-08 13:10:58 +0200

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,70 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+template <typename T>
+
+class Stack
+{
+    private:
+        vector<T> v;
+
+    public:
+        bool Empty()
+        {
+            return v.empty();
+        }
+
+        void push(const T &item)
+        {
+            v.push_back(item);
+        }
+
+        void pop()
+        {
+            if (v.empty()==0)
+                v.pop_back();
+            else
+                cout<<"\nStiva este goala!";
+        }
+
+        T& top()
+        {
+            if (v.empty()==0)
+                return v.back();
+            else
+                cout<<"\nStiva este goala!";
+        }
+
+        void afisare()
+        {
+            int i;
+            for(i=v.size()-1; i>=0;i--)
+                cout<<v.at(i)<<endl;
+        }
+};
+
+int main()
+{
+    Stack<int> s;
+
+    if( s.Empty()==1)
+        cout<<"Stiva este goala.";
+    else
+        cout<<"Stiva nu este goala.";
+
+    cout<<"\nAdaug in stiva elementele: 10, 4, -2, 1\n";
+    s.push(10);
+    s.push(4);
+    s.push(-2);
+    s.push(1);
+    s.afisare();
+    cout<<"\nVarful stivei: ";
+    cout<<s.top();
+    cout<<"\n\nEliminarea ultimului element din stiva:\n";
+    s.pop();
+    s.afisare();
+
+    return 0;
+}
